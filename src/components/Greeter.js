886f2faba96c236c594d8042f78f22de71a653af
xingbo zhang <zxb@ekauibao.com>
2018-09-13 15:32:35 +0800

    init

---
+++ src/components/Greeter.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react'
+export default class Greeter extends Component{
+    render(){
+        return <div>HELLO</div>
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import {render} from 'react-dom'
+import Greeter from './components/Greeter'
+
+render(<Greeter/>, document.getElementById('root'))
