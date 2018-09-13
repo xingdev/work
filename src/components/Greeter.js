40781c3329550a6781435bb2b1409bb240e2f7cb
xingbo zhang <zxb@ekauibao.com>
2018-09-13 15:53:50 +0800

    test

--- src/components/Greeter.js
+++ src/components/Greeter.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: xingdev 
+ * @Date: 2018-09-13 15:50:11 
+ * @Last Modified by:   xingdev 
+ * @Last Modified time: 2018-09-13 15:50:11 
+ */
 import React, {Component} from 'react'
 export default class Greeter extends Component{
     render(){
---
+++ src/components/Input.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: xingdev 
+ * @Date: 2018-09-13 15:50:42 
+ * @Last Modified by:   xingdev 
+ * @Last Modified time: 2018-09-13 15:50:42 
+ */
