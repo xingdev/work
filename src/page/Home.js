4917f97d0931f3fb009725cea832a86574451d72
xingbo zhang <zxb@ekauibao.com>
2018-09-17 18:57:58 +0800

    哦哦哦

--- src/page/Home.js
+++ src/page/Home.js
@@ -2,7 +2,7 @@
  * @Author: xingdev 
  * @Date: 2018-09-13 16:42:09 
  * @Last Modified by: xingdev
- * @Last Modified time: 2018-09-17 15:00:07
+ * @Last Modified time: 2018-09-17 17:11:39
  */
 
 import React, { Component } from "react";
@@ -14,6 +14,7 @@ import Greeter from "./Greeter";
 import Login from "./Login";
 import LOGO_SVG from "../assets/logo.svg";
 import { Layout, Menu, Button } from "antd";
+import m from "../lib/map";
 const { Header, Content, Footer } = Layout;
 const store = observable({
   isAuth: false,
@@ -56,10 +57,10 @@ export default class Home extends Component {
                 style={{ lineHeight: "64px" }}
               >
                 <Menu.Item>
-                  <Link to="./">Home</Link>
+                  <Link to="/">Home</Link>
                 </Menu.Item>
                 <Menu.Item>
-                  <Link to="./login">Login</Link>
+                  <Link to="/login">Login</Link>
                 </Menu.Item>
               </Menu>
               <AuthButton />
--- src/page/Login.js
+++ src/page/Login.js
@@ -2,7 +2,7 @@
  * @Author: xingdev 
  * @Date: 2018-09-13 15:50:11 
  * @Last Modified by: xingdev
- * @Last Modified time: 2018-09-14 19:14:01
+ * @Last Modified time: 2018-09-17 17:33:22
  */
 import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
@@ -23,6 +23,11 @@ export default class Login extends Component {
     }
   };
 
+  test = e => {
+    const reg = /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+    console.log(reg.test(e.target.value));
+  };
+
   render() {
     if (this.props.store.isAuth) {
       return <Redirect to={{ pathname: "/" }} />;
@@ -30,12 +35,21 @@ export default class Login extends Component {
     return (
       <div style={{ width: 300 }}>
         <h3>Login</h3>
-        <Row gutter={16}>
+        <Row gutter={16} style={{ marginBottom: "10px" }}>
           <Col span={4}>name:</Col>
           <Col span={16}>
             <Input onChange={this.onChange} />
           </Col>
-          <Col span={4}>
+          <Col span={4} />
+        </Row>
+        <Row gutter={16} style={{ marginBottom: "10px" }}>
+          <Col span={4}>phone:</Col>
+          <Col span={16}>
+            <Input onChange={this.test} />
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={16}>
             <Button onClick={this.onLogin} type="primary">
               login
             </Button>
